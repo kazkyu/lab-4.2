c92ba957a03ffc43d518d7030f57b5fea93ffbf0
kazkyu <nguyendangkhoa2703@gmail.com>
2016-04-13 12:40:52 +0300

    method changeBalance

--- MyApp.java
+++ MyApp.java
@@ -1,7 +1,7 @@
 //Author: Trinh Ky Nam
 public class MyApp {
  public static void main(String[] args) {
- Product product = new Product("Edam", 3.3, 120);
+ Product product = new Product("Edam", 3.3, 120, 200);
  System.out.println("Product value is " + product.countValue());
  product.printProduct();
  }
@@ -10,10 +10,12 @@ class Product {
 private String name;
 private double price;
 private int amount; //Amount in storage
-public Product(String name, double price, int amount) {
+private int newValue;
+public Product(String name, double price, int amount, int newValue) {
 this.name = name;
 this.price = price;
 this.amount = amount;
+this.newValue = newValue;
 }
 public double countValue() {
 return(amount * price);
@@ -21,5 +23,8 @@ return(amount * price);
 public void printProduct() {
 System.out.printf("Product %s, price %4.1f and balance %d pcs",
 name,price,amount);
+}
+public int changeBalance(int newvalue){
+ return this.newValue = newvalue * amount;  
 }
 }
